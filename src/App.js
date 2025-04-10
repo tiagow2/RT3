a0f8957776810e0e54f20f81f7abf1868ff23ec7
tiagow2 <tiago.bala02@gmail.com>
2025-04-10 19:19:58 -0300

    Update

---
+++ src/App.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const OperacoesMatematicas_1 = require("./calculadoraClass/OperacoesMatematicas");
+const a = 20;
+const b = 5;
+console.log(`Soma: ${OperacoesMatematicas_1.OperacoesMatematicas.somar(a, b)}`);
+console.log(`Subtração: ${OperacoesMatematicas_1.OperacoesMatematicas.subtrair(a, b)}`);
+console.log(`Multiplicação: ${OperacoesMatematicas_1.OperacoesMatematicas.multiplicar(a, b)}`);
+console.log(`Divisão: ${OperacoesMatematicas_1.OperacoesMatematicas.dividir(a, b)}`);
